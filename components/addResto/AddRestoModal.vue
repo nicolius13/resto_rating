e5d17d91d151df189c7bd1823d9d7bf30bdc6cff
Nicolas Vastrade <nicolas.vastrade@gmail.com>
2020-04-30 08:42:24 +0700

    modals style to main.css

--- components/addResto/AddRestoModal.vue
+++ components/addResto/AddRestoModal.vue
@@ -25,6 +25,7 @@
           id="name-input"
           v-model="name"
           :state="nameState"
+          class="textInput"
           required
         ></b-form-input>
       </b-form-group>
@@ -80,62 +81,4 @@ export default {
 };
 </script>
 
-<style>
-/* not scoped for the general modal style */
-.modal-header {
-  border-bottom: 1px solid #292929;
-}
-.modal-footer {
-  border-top: none;
-}
-.modal-content {
-  background-color: #1d1d1d;
-  color: #fff;
-}
-
-.close {
-  color: #ff2e63;
-  text-shadow: none;
-  opacity: 1;
-}
-.close:hover {
-  color: #ff2e63;
-}
-</style>
-
-<style scoped>
-/* scoped for the inside of the modal */
-
-.modalTitle {
-  color: #ff2e63;
-}
-
-#name-input {
-  background-color: #292929;
-  border: 1px solid #000;
-  color: #fff;
-}
-#name-input:focus {
-  box-shadow: 0 0 0 0.2rem rgba(8, 217, 214, 0.15);
-}
-
-/* Buttons */
-
-.cancelBtn {
-  color: #838383;
-  border: 1px solid #838383;
-}
-.cancelBtn:hover {
-  background-color: #292929;
-  color: #fff;
-}
-
-.okBtn {
-  color: #ff2e63;
-  border: 1px solid #ff2e63;
-}
-.okBtn:hover {
-  background-color: #ff2e63;
-  color: #fff;
-}
-</style>
+<style scoped></style>
--- components/restoList/AddCommentModal.vue
+++ components/restoList/AddCommentModal.vue
@@ -14,7 +14,11 @@
         <StarPicker id="rating-input" @starPick="rating = $event" />
       </b-form-group>
       <b-form-group label="Comment" label-for="comment-input">
-        <b-form-textarea id="comment-input" v-model="comment"></b-form-textarea>
+        <b-form-textarea
+          id="comment-input"
+          v-model="comment"
+          class="textInput"
+        ></b-form-textarea>
       </b-form-group>
     </b-form>
     <template v-slot:modal-footer="{ ok, cancel }">
@@ -70,62 +74,4 @@ export default {
 };
 </script>
 
-<style>
-/* not scoped for the general modal style */
-.modal-header {
-  border-bottom: 1px solid #292929;
-}
-.modal-footer {
-  border-top: none;
-}
-.modal-content {
-  background-color: #1d1d1d;
-  color: #fff;
-}
-
-.close {
-  color: #ff2e63;
-  text-shadow: none;
-  opacity: 1;
-}
-.close:hover {
-  color: #ff2e63;
-}
-</style>
-
-<style scoped>
-/* scoped for the inside of the modal */
-
-.modalTitle {
-  color: #ff2e63;
-}
-
-#comment-input {
-  background-color: #292929;
-  border: 1px solid #000;
-  color: #fff;
-}
-#comment-input:focus {
-  box-shadow: 0 0 0 0.2rem rgba(8, 217, 214, 0.15);
-}
-
-/* Buttons */
-
-.cancelBtn {
-  color: #838383;
-  border: 1px solid #838383;
-}
-.cancelBtn:hover {
-  background-color: #292929;
-  color: #fff;
-}
-
-.okBtn {
-  color: #ff2e63;
-  border: 1px solid #ff2e63;
-}
-.okBtn:hover {
-  background-color: #ff2e63;
-  color: #fff;
-}
-</style>
+<style scoped></style>
